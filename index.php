c666d7267ed4b5eb0dc88b0979358b6ae00dd159
DCMPSDE\egonzalez <emilrichardo@gmail.com>
2017-11-08 11:58:13 -0300

    Ajustes responsive para home

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 
 
-<section class="welcome bg-primary  text-light h-100 ">
+<section class="welcome bg-primary  text-light h-100 py-md-5 py-4">
 
 	<div class="fullscreen-bg">
     <video loop muted autoplay poster="img/videoframe.jpg" class="fullscreen-bg__video">
@@ -11,20 +11,20 @@
         
     </video>
 </div>
-		<div class="container">
+		<div class="container py-md-5">
 			<div class="row justify-content-center text-center">
 				<div class="col-md-8 col-11">
 					<h2 class="font-weight-light">¡Bienvenidos al sitio web del Ministerio Público Fiscal de Santiago del Estero!</h2>
-					<p class="lead my-4 d-none d-md-inline-block">
+					<p class="lead my-4  mb-3 mb-md-0">
 					Conozca los servicios que brinda el organismo tanto a las víctimas y testigos de delitos como a todos aquellos que quieran conocer el trabajo del MPF.</p>
 					
-						<a href="<?php echo home_url('/'); ?>el-ministerio" class="btn  btn-outline-light mt-md-0 mt-2">
+						<a href="<?php echo home_url('/'); ?>el-ministerio" class="btn  btn-outline-light mt-md-0 mt-2 d-md-inline-block d-block">
 							<i class="fa fa-plus-circle"></i> Sobre el MPF
 						</a>
-						<a href="<?php echo home_url('/'); ?>el-ministerio/circunscripciones" class="btn  btn-outline-light mt-md-0 mt-2">
+						<a href="<?php echo home_url('/'); ?>el-ministerio/circunscripciones" class="btn  btn-outline-light mt-md-0 mt-2 d-md-inline-block d-block">
 							<i class="fa fa-map-marker"></i> Circunscripciones
 						</a>
-						<a href="<?php echo home_url('/'); ?>noticias" class="btn  btn-success mt-md-0 mt-2">
+						<a href="<?php echo home_url('/'); ?>noticias" class="btn  btn-success mt-md-0 mt-2 d-md-inline-block d-block">
 							<i class="fa  fa-newspaper-o"></i> Sección Noticias
 						</a>
 					
